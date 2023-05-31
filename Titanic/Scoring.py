b2a58a63c9c6d31793786c06cf5bed6945a89fee
Dariusz Litwinski <litwinski.dariusz42@gmail.com>
2023-05-31 20:40:40 +0200

    Work done on plots to improve clarity

--- Titanic/Scoring.py
+++ Titanic/Scoring.py
@@ -8,9 +8,9 @@ path=r'C:\Users\Darek\Documents\Magisterka\PracaMagPreproccessing\Results'
 
 def initialize_result_file():
     file = open(path + "/results.csv", "w")
-    file.write("org_dataset,dataset,scenario,xgb_score,rfc_score,knn_score\n")
+    file.write("org_dataset,dataset,group,scenario,xgb_score,rfc_score,knn_score\n")
 
-def score(dataset,suffix,scenario,X_train,y_train,X_test,y_test):
+def score(dataset,suffix,group,scenario,X_train,y_train,X_test,y_test):
     print("========= " + dataset + "_"+ str(suffix) + " =========")
     print("Scenario: " + scenario)
     xgb = xgboost.XGBClassifier(n_estimators=10)
@@ -26,4 +26,4 @@ def score(dataset,suffix,scenario,X_train,y_train,X_test,y_test):
     knn_score = knn.score(X_test, y_test)
     print("KNeighbors Classifier: " + str(knn_score)) 
     file = open(path + "/results.csv", "a")
-    file.write(dataset+","+dataset+'_'+str(suffix)+","+scenario+","+str(xgb_score)+","+str(rfc_score)+","+str(knn_score)+"\n")
+    file.write(dataset+","+dataset+'_'+str(suffix)+","+group+","+scenario+","+str(xgb_score)+","+str(rfc_score)+","+str(knn_score)+"\n")
